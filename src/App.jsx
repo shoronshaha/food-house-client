b827e1a693ebec32d636949db06eaba16f9d9e91
Smaran Shaha <smaranshaha@gmail.com>
2024-12-03 16:21:35 +0600

    project setup and navbar , footer are added too

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+    </>
+  );
+}
+
+export default App;
