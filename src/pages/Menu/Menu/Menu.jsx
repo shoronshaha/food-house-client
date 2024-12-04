98c5f3c8e5ca83661d74e9f324732b66fbc30a29
Smaran Shaha <smaranshaha@gmail.com>
2024-12-04 19:07:13 +0600

    use costome hook for multiple thing and added our manu cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,7 +9,8 @@ const Menu = () => {
       <Helmet>
         <title>FOOD | MENU</title>
       </Helmet>
-      <h2> Menu</h2>
+      <Cover img={menuImg} title="our menu"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
