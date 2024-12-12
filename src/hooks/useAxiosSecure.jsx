b6eb84c22a25b9e224b848af6dd052b2e43df304
Smaran Shaha <smaranshaha@gmail.com>
2024-12-12 23:00:05 +0600

    added interceptors for 401 and 403 error status for authrization

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,29 @@ const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request stopped by interceptors", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      //Do something with request error
+      return Promise.reject(error);
+    }
+  );
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    (error) => {
+      console.log("status error in the interceptor", error);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
