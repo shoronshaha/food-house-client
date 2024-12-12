1462ba2fb05f4039e4137a73da8577f33bbbfd98
Smaran Shaha <smaranshaha@gmail.com>
2024-12-12 21:40:34 +0600

    adde jwt token and api use for user validation and secure user data

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -11,6 +11,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -19,6 +20,7 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const googleProvider = new GoogleAuthProvider();
+  const axiosPublic = useAxiosPublic();
 
   console.log("user", user);
   const createUser = (email, password) => {
@@ -54,8 +56,15 @@ const AuthProvider = ({ children }) => {
       console.log("current user", currentUser);
       if (currentUser) {
         // get token and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
       } else {
         // todo remove token(it token stored in the client side: Local storage,caching,in memory)
+        localStorage.removeItem("access-token");
       }
       setLoading(false);
     });
